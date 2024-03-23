d21a084e0c558910a46d89bddd0c42cbf9d5f5cc
boushra-chl <bchilouch98@gmail.com>
2024-03-23 21:23:23 +0000

    update1

--- models/base_model.py
+++ models/base_model.py
@@ -10,16 +10,15 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         if len(kwargs) > 0:
             for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                if key == "created_at":
-                    kwargs['created_at'] = datetime.strptime(kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
-                if key == "updated_at":
-                    kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, key, value)
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
@@ -28,8 +27,8 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        obj_dict = {**self.__dict__}
+        obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = type(self).__name__
-        obj_dict['created_at'] = obj_dict['created_at'].isoformat()
-        obj_dict['updated_at'] = obj_dict['updated_at'].isoformat()
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
