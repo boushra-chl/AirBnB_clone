31bd34b9056f28638a37a8878da30c342015120d
boushra-chl <bchilouch98@gmail.com>
2024-03-23 20:50:48 +0000

    update

--- models/base_model.py
+++ models/base_model.py
@@ -7,22 +7,29 @@ from datetime import datetime
 
 class BaseModel:
     """Class that defines all common attributes/methods for other classes"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        if len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key == "created_at":
+                    kwargs['created_at'] = datetime.strptime(kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
+                if key == "updated_at":
+                    kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
         self.id = str(uuid4())
         self.created_at = datetime.now()
-        self.updated_at = None
-        self.add_class_name("__class__", self.__class__.__name__)
+        self.updated_at = datetime.now()
 
     def __str__(self):
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
 
-    def add_class_name(self, key, value):
-        self.__dict__[key] = value
-
     def to_dict(self):
-        self.created_at = self.created_at.isoformat()
-        self.updated_at = self.updated_at.isoformat()
-        return self.__dict__
+        obj_dict = {**self.__dict__}
+        obj_dict['__class__'] = type(self).__name__
+        obj_dict['created_at'] = obj_dict['created_at'].isoformat()
+        obj_dict['updated_at'] = obj_dict['updated_at'].isoformat()
+        return obj_dict
