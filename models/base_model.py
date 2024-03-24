2acf8771636c3047a2e8afad3fcf268483e00bf3
boushra-chl <bchilouch98@gmail.com>
2024-03-24 21:53:07 +0000

    task52

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@
 
 from uuid import uuid4
 from datetime import datetime
+from models import storage
 
 class BaseModel:
     """Class that defines all common attributes/methods for other classes"""
@@ -19,12 +20,14 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         obj_dict = self.__dict__.copy()
