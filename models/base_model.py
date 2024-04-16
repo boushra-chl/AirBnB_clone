eddec393681a993cbfee8352b51293b9ffdd7285
coushra-chl <bchilouch1998@gmail.com>
2024-04-16 13:42:00 +0100

    ok

--- models/base_model.py
+++ models/base_model.py
@@ -28,6 +28,7 @@ class BaseModel:
     def save(self):
         self.updated_at = datetime.now()
         models.storage.save()
+        print(OK)
 
     def to_dict(self):
         obj_dict = self.__dict__.copy()
