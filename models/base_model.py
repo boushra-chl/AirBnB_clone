98c69fe96ed32e51168ba0991ae7def648df7730
boushra-chl <bchilouch1998@gmail.com>
2024-04-20 18:00:22 +0100

    cc

--- models/base_model.py
+++ models/base_model.py
@@ -1,10 +1,11 @@
+#!/usr/bin/python3
 from uuid import uuid4
 from datetime import datetime
 from models import storage
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
-        if kwargs != 0:
+        if kwargs:
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key in ['created_at', 'updated_at']:
@@ -12,13 +13,13 @@ class BaseModel:
                     else:
                         setattr(self, key, value)
         else:
-            self.id = str(uuid.uuid4())
+            self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
         
     def __str__(self):
-        return f"[{self.__class__.name}] ({self.id}) {self.__dict__}"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
         
     def save(self):
         self.updated_at = datetime.now()
@@ -29,4 +30,4 @@ class BaseModel:
         my_dict['__class__'] = self.__class__.__name__
         my_dict['created_at'] = self.created_at.isoformat()
         my_dict['updated_at'] = self.updated_at.isoformat()
-        return my_dict
+        return my_dicti
