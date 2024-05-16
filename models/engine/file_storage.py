33ab5ac9302721df35a7cb34204f29174028b1e6
boushra-chl <bchilouch1998@gmail.com>
2024-05-16 20:25:09 +0100

    base model

---
+++ models/engine/file_storage.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        json_data ={}
+        for key, obj in self.__objects.items():
+            json_data[key] = obj.to_dict()
+        with open(self.__file_path, 'w') as json_file:
+            json.dump(json_data, json_file)
+
+    def reload(self):
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, 'r') as json_file:
+                json_data = json.load(json_file)
+                for key, value in json_data.items():
+                    class_name, obj_id = key.split('.')
+                    obj = globals()[class_name](**value)
+                    self.__objects[key] = obj
