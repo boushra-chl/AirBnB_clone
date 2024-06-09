286d514dcee4534214f37017c0c2ab9cb42ff88c
boushra-chl <bchilouch1998@gmail.com>
2024-06-09 18:13:59 +0100

    fix user 1

--- models/user.py
+++ models/user.py
@@ -8,7 +8,7 @@ from models.base_model import BaseModel
 class User(BaseModel):
     """class User that inherits from BaseModel"""
 
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
